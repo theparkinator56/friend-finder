69ac3b81167232574dcb5fd0e9e4fb4856d046a5
TheFreeMan <theparkinator56@gmail.com>
2018-11-15 19:54:59 -0600

    trying to find repo

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -6,10 +6,6 @@ module.exports = function(app) {
 	res.json(survey);
    });
 
-    app.get("/api/friends", function(req,res) {
-   res.json(friends);
-   });
-
    app.post("/api/friends", function(req,res) {
     var newFriend = req.body;
    	console.log(req.body);
